33c05bef21b282d230a7f5da35639b37a91f68fd
jotaleiva <jleivam@fen.uchile.cl>
2020-03-09 21:37:48 -0300

    primer commit de mi sitio web

---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+// Image switcher code
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/box-model.png') {
+    myImage.setAttribute ('src','images/learn-js.png');
+  } else {
+    myImage.setAttribute ('src','images/box-model.png');
+  }
+}
+
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'BICE is cool, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'BICE is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
